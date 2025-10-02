4c530d7e5ee0130c72431d147901c905c15830df
Srilatha Maddineni <srilatha.maddineni17@gmail.com>
2025-10-01 22:38:11 -0400

    Initial commit for blog project

---
+++ blog/src/test/java/com/devtiro/blog/BlogApplicationTests.java
@@ -0,0 +1,14 @@
+package com.devtiro.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+
+	}
+
+}
